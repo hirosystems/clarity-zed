3b22a49464963b71b4b2247456672541c355f33f
Jeff Bencin <jeff.bencin@gmail.com>
2025-05-20 15:41:20 -0400

    feat: Add more syntax highlights

--- languages/clarity/highlights.scm
+++ languages/clarity/highlights.scm
@@ -1,4 +1,4 @@
-;; Comments
+; Comments
 (comment) @comment
 
 ; Punctuation
@@ -15,7 +15,7 @@
   ","
 ] @punctuation.delimiter
 
-;; Literals
+; Literals
 (bool_lit) @boolean
 
 [
@@ -33,3 +33,61 @@
   (standard_principal_lit)
   (contract_principal_lit)
 ] @string.special
+
+; Types
+[
+  (native_type)
+  (trait_type)
+] @type
+
+; Keywords
+[
+  "some"
+  "ok"
+  "err"
+  (list_lit_token)
+] @keyword
+
+[
+  "+"
+  "-"
+  "*"
+  "/"
+  "mod"
+  "pow"
+  "<"
+  "<="
+  ">"
+  ">="
+  "and"
+  "or"
+  "xor"
+] @keyword.operator
+
+; Functions
+(function_signature (identifier) @function)
+(function_signature_for_trait (identifier) @function)
+(basic_native_form operator: (native_identifier) @function)
+(contract_function_call operator: (identifier) @function)
+
+[
+  "let"
+  "impl-trait"
+  "use-trait"
+  "define-trait"
+  "define-read-only"
+  "define-private"
+  "define-public"
+  "define-data-var"
+  "define-fungible-token"
+  "define-non-fungible-token"
+  "define-constant"
+  "define-map"
+] @function
+
+; Variables and properties
+(global) @variable.special
+
+(tuple_lit key: (identifier) @property)
+(tuple_type key: (identifier) @property)
+(tuple_type_for_trait key: (identifier) @property)
