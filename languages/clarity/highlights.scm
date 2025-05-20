483e35d103dd3408eeb3037d2bf2b07df2255054
Jeff Bencin <jeff.bencin@gmail.com>
2025-05-20 13:10:49 -0400

    feat: Basic syntax highlighting (comments, literals, and punctuation)

---
+++ languages/clarity/highlights.scm
@@ -0,0 +1,35 @@
+;; Comments
+(comment) @comment
+
+; Punctuation
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "<"
+  ">"
+] @punctuation.bracket
+
+[
+  ","
+] @punctuation.delimiter
+
+;; Literals
+(bool_lit) @boolean
+
+[
+  (int_lit)
+  (uint_lit)
+] @number
+
+[
+  (ascii_string_lit)
+  (utf8_string_lit)
+] @string
+
+[
+  (buffer_lit)
+  (standard_principal_lit)
+  (contract_principal_lit)
+] @string.special
